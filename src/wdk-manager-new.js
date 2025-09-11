1d3145c5408ef8e22d24f5ee5c9abdabfa19c998
Claudio <claudiocarvalhovilasboas@gmail.com>
2025-09-10 23:34:14 -0300

    Update src/wdk-manager-new.js
    Co-authored-by: Davide Casale <davi0kprogramsthings@gmail.com>

--- src/wdk-manager-new.js
+++ src/wdk-manager-new.js
@@ -26,7 +26,8 @@ import WdkWallet from '@wdk/wallet'
  * const wdk = new WdkManager('test only example nut use this real life secret phrase must random')
  * wdk.registerWallet('ethereum', WalletManagerEvm, { rpcUrl: 'https://yourURL' })
  * const account = await wdk.getAccount('ethereum', 0)
- * console.log(await account.getAddress())
+ * // Output: "Account m/44'/60'/0'/0/0: 0x123..."
+ * console.log("Account m/44'/60'/0'/0/0:", await account.getAddress())
  */
 class WdkManager {
   /**
